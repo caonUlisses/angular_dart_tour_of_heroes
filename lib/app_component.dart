15b07a5f9bcb3f64cfa0d7c612f5ccf0c0b613ce
Ulisses Caon <ulissescaon@gmail.com>
2018-04-30 08:14:48 -0300

    Fix typo | Remove unused import

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,5 +1,4 @@
 import 'package:angular/angular.dart';
-import 'package:angular_forms/angular_forms.dart';
 import 'src/hero.dart';
 import 'src/mock_heroes.dart';
 import 'src/hero_detail_component.dart';
@@ -12,7 +11,7 @@ import 'src/hero_detail_component.dart';
 )
 class AppComponent {
   Hero selectedHero;
-  final Sting title = 'Tour of Heroes';
+  final String title = 'Tour of Heroes';
   List<Hero> heroes = mockHeroes;
   void onSelect(Hero hero) => selectedHero = hero;
 }
